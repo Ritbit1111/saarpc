deadb3125df1eeb6098b4b1ebdddc2b92cf2ed9d
Ritesh <ritesh.play1@gmail.com>
2018-08-01 11:56:30 +0530

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -62,3 +62,62 @@ streamField = u * R.y - v * R.x
 print "Stream function", integrateGradient(streamField)
 
 plt.show()
+
+        
+        
+        
+        
+        ########################################new#######################################
+        import numpy as np
+import matplotlib.pyplot as plt
+    
+#length of the plates of the capacitor
+x1,y1 = 3,3
+x2,y2 = 3,5
+lx=4
+ly=4
+#The capacitor is placed on a bigger box of size N1xN2
+Nx=6*lx
+Ny=6*ly
+x=np.linspace(0,Nx-1,num=Nx)
+y=np.linspace(0,Ny-1,num=Ny)
+
+X,Y = np.meshgrid(x,y)
+
+#Potential is 2D variable
+V=np.zeros((Nx,Ny))
+
+V[x1:x1+lx,y1]=6.5
+V[x2,y2:y2+ly]=6.5
+#print(V)
+#Potential at each point is the average of its neighbor barring the boundary codidtions
+iter=1000
+
+count=0
+while count<iter:
+    for i in range(1,Nx-2):
+        for j in range(1,Ny-2):
+            if i>=x1 and i<x1+lx and j==y1:
+                #print(i,j,V[i][j])
+                continue
+            if j>=y2 and j<y2+ly and i==x2:
+                #print(i,j,V[i][j])
+                continue
+            V[i][j]=(V[i-1][j]+V[i+1][j]+V[i][j-1]+V[i][j+1])/4
+    count=count+1
+print(V)
+
+fig = plt.figure(figsize=(6,6))
+
+ax = fig.add_subplot(111)
+ax.set_title('colorMap')
+plt.imshow(V)
+ax.set_aspect('equal')
+
+cax = fig.add_axes([0.12, 0.1, 0.78, 0.8])
+cax.get_xaxis().set_visible(False)
+cax.get_yaxis().set_visible(False)
+cax.patch.set_alpha(0)
+cax.set_frame_on(False)
+plt.colorbar(orientation='vertical')
+plt.show()
